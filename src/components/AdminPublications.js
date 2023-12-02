0f2779aa8c02d5059b0a5bb6e8b473231db35311
= <antoniochavezmarco@gmail.com>
2023-12-01 20:02:11 -0600

    Admin verification everytime /admin/posts is made

--- src/components/AdminPublications.js
+++ src/components/AdminPublications.js
@@ -1,6 +1,26 @@
 import SearchPublication from "./SearchPublication";
 import FormImage from "./FormImage";
+import config from "./config.json";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Cookies from "js-cookie";
 function AdminPublications() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const isAdmin = async () => {
+            const response = await axios.post(`${config.endpoint}/admin`,{
+                cookie: Cookies.get('session')
+            });
+            if(response.data.success === false){
+                Cookies.remove('session');
+                Cookies.remove('role');
+                Cookies.remove('username');
+                window.location.reload();
+                navigate('/');
+            }
+        }
+    }, [config.endpoint, navigate]);
     return ( 
         <section id="manage-publications" className="section-admin">
             <h2>Gestión de publicaciones</h2>
